65bf7c25cf1284fa9327d7065332a3ebda45c8f1
boomshakazulu <dallonlahoda@hotmail.com>
2024-10-25 02:29:28 -0600

    removed logs

--- client/src/components/Cart/index.jsx
+++ client/src/components/Cart/index.jsx
@@ -13,7 +13,6 @@ const Cart = () => {
   const navigate = useNavigate();
 
   const cartRef = useRef();
-  console.log(state.cart);
   useEffect(() => {
     async function getCart() {
       const cart = await idbPromise("cart", "get");
